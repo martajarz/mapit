f9411f77ddbcd89f6a9b6739aaa2775c23a08d70
martajarz <martajarz@wp.pl>
2017-06-08 22:50:48 +0200

    Added some styling.

--- js/map.js
+++ js/map.js
@@ -2,13 +2,15 @@ var map;
 var infoBubble;
 var messageBubble;
 var point;
+var mapstyles;
 
 function initMap() {
     var start = {lat: 54.352156, lng: 18.645932};
     
     map = new google.maps.Map(document.getElementById('map'), {
         center: start,
-        zoom: 15
+        zoom: 15,
+        styles: mapstyles
     });
 
     infoBubble = new google.maps.InfoWindow({
@@ -36,4 +38,157 @@ function saveData() {
     infoBubble.close();
     messageBubble.open(map, point);
 };
+
+mapstyles = [
+    {
+        "featureType": "all",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#0090ee"
+            }
+        ]
+    },
+    {
+        "featureType": "all",
+        "elementType": "labels.text",
+        "stylers": [
+            {
+                "weight": "1"
+            }
+        ]
+    },
+    {
+        "featureType": "all",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "gamma": 0.01
+            },
+            {
+                "lightness": "13"
+            },
+            {
+                "weight": "1"
+            }
+        ]
+    },
+    {
+        "featureType": "all",
+        "elementType": "labels.text.stroke",
+        "stylers": [
+            {
+                "saturation": "-26"
+            },
+            {
+                "lightness": -33
+            },
+            {
+                "weight": 2
+            },
+            {
+                "gamma": 0.8
+            },
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "all",
+        "elementType": "labels.icon",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "landscape",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "lightness": "21"
+            },
+            {
+                "saturation": "53"
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "saturation": "55"
+            },
+            {
+                "lightness": "2"
+            }
+        ]
+    },
+    {
+        "featureType": "poi.park",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "lightness": "31"
+            },
+            {
+                "saturation": "100"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "lightness": "-2"
+            },
+            {
+                "saturation": "7"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "geometry.stroke",
+        "stylers": [
+            {
+                "saturation": 25
+            },
+            {
+                "lightness": 25
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "labels.text.stroke",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "all",
+        "stylers": [
+            {
+                "lightness": "-10"
+            },
+            {
+                "saturation": "57"
+            },
+            {
+                "gamma": "0.95"
+            },
+            {
+                "color": "#0081e5"
+            }
+        ]
+    }
+]
 
